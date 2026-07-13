e9e0ae6eb578fca42212211e92d091a53a3d09e6
art0007i <art0007i@gmail.com>
2026-07-13 06:24:52 +0200

    BepisLoader port

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.NET.Common;
+using BepInExResoniteShim;
+using Elements.Core;
+using FrooxEngine;
+using HarmonyLib;
+
+namespace ConsistentDashboard;
+
+[ResonitePlugin(PluginMetadata.GUID, PluginMetadata.NAME, PluginMetadata.VERSION, PluginMetadata.AUTHORS, PluginMetadata.REPOSITORY_URL)]
+[BepInDependency(BepInExResoniteShim.PluginMetadata.GUID, BepInDependency.DependencyFlags.HardDependency)]
+public class Plugin : BasePlugin
+{
+#nullable disable
+    internal static new ManualLogSource Log;
+    internal static ConfigEntry<bool> Enabled;
+    internal static ConfigEntry<int2> Resolution;
+#nullable enable
+
+    public override void Load()
+    {
+        Log = base.Log;
+
+        Enabled = Config.Bind("General", "Enabled", true,
+            "When enabled the mod will set the dashboard size to a set resolution.");
+        Resolution = Config.Bind("General", "Resolution", new int2(2560, 1440),
+            "<color=orange>⚠Messing with this option may result in brekaing your dash.⚠</color> The resolution the dashboard will be set to while the mod is enabled.");
+        
+        HarmonyInstance.PatchAll();
+    }
+    
+    [HarmonyPatch(typeof(RadiantDash), nameof(RadiantDash.AutoAdaptWidth), MethodType.Getter)]
+    class ConsistentDashboardPatch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            if (!Enabled.Value) return true;
+            __result = false;
+            return false;
+        }
+    }
+    [HarmonyPatch(typeof(RadiantDash), nameof(RadiantDash.BASE_RESOLUTION), MethodType.Getter)]
+    class ConsistentDashboardResolutionPatch
+    {
+        public static bool Prefix(ref int2 __result)
+        {
+            if (!Enabled.Value) return true;
+            __result = Resolution.Value;
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # ConsistentDashboard
+[![Thunderstore Badge](https://modding.resonite.net/assets/available-on-thunderstore.svg)](https://thunderstore.io/c/resonite/)
 
-A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that makes it so the dashboard is always the same resolution in VR and in desktop mode.
+A [Resonite](https://resonite.com/) mod that makes it so the dashboard is always the same resolution in VR and in desktop mode.
 
 Also has a config option that allows changing this resolution.
 
 This mod fixes [#2431](https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/2431)
 
-## Installation
-1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
-1. Place [ConsistentDashboard.dll](https://github.com/art0007i/ConsistentDashboard/releases/latest/download/ConsistentDashboard.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
-1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
+## Installation (Manual)
+1. Install [BepisLoader](https://github.com/ResoniteModding/BepisLoader) for Resonite.
+2. Download the latest release ZIP file (e.g., `art0007i-ConsistentDashboard-1.0.0.zip`) from the [Releases](https://github.com/art0007i/ConsistentDashboard/releases) page.
+3. Extract the ZIP and copy the `plugins` folder to your BepInEx folder in your Resonite installation directory:
+   - **Default location:** `C:\Program Files (x86)\Steam\steamapps\common\Resonite\BepInEx\`
+4. Start the game. If you want to verify that the mod is working you can check your BepInEx logs.
